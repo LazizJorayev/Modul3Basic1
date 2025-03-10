f57f7aec1a36e86c4927db93719f875f61250105
pc <pc@DESKTOP-8SL5GCF>
2025-03-11 02:59:25 +0500

    Add project files.

---
+++ Strings/Program.cs
@@ -0,0 +1,38 @@
+﻿////Misol_1
+//string str = Console.ReadLine();
+//Console.Write("Enter n = ");
+//int n = int.Parse(Console.ReadLine());
+//if (str.Length < n)
+//    str = str.ToUpper();
+//str = str.ToLower();
+//Console.WriteLine(str);
+
+////Misol_2
+//int x, y;
+//string result;
+//x = int.Parse(Console.ReadLine());
+//y = int.Parse(Console.ReadLine());
+//if (x > y)
+//    result = $"{x} katta {y} dan";
+//else if (x < y)
+//    result = $"{x} kichik {y} dan";
+//else if (x == y)
+//    result = $"{x} teng {y} ";
+//else
+//    result = $"{x} va {y} son emas";
+//Console.WriteLine(result);
+
+////Misol_3
+//string kun;
+//kun = Console.ReadLine();
+//switch (kun)
+//{
+//    case "dushanba":   Console.Write("Monday");break;
+//    case "seshanba":   Console.Write("Tuesday");break;
+//    case "chorshanba": Console.Write("Wednesday");break;
+//    case "payshanba":  Console.Write("Thursday");break;
+//    case "juma":       Console.Write("Friday");break;
+//    case "shanba":     Console.Write("Saturday");break;
+//    case "yakshanba":  Console.Write("Oddix");break;
+//    default: Console.WriteLine("Bunday kun yuq");break;
+}
